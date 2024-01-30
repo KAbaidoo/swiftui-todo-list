e2d77f4eddb159c08c5f9668fe410a307c5c9324
Kelvin Abaidoo <kabaidoo@hotmail.com>
2024-01-30 09:38:42 +0000

    Initial Commit

---
+++ TodoList/TodoListApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoListApp.swift
+//  TodoList
+//
+//  Created by Kelvin Abaidoo on 30/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct TodoListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
